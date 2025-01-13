8628df26c21f8a0a9de9e3f5aa2538a60193d9f7
Augustine Gyan <augustinehappysunshine369@gmail.com>
2025-01-13 23:03:55 +0000

    login

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -54,7 +54,7 @@ const signup_post = async (req,res)=>{
        const user = await User.create({email,password})
        const token = createToken(user._id);
        res.cookie('jwt',token,{httpOnly: true, maxAge:maxAge*1000})
-       res.status(201).json(user)
+       res.status(201).json({user:user._id})
         
     } catch (err) {
         const errors = handleErrors(err)
