cb14a80902f79583451b67733cf1aab771b33321
Augustine Gyan <augustinehappysunshine369@gmail.com>
2025-01-14 18:02:29 +0000

    protected Routes

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const connectToDb = require('./database/dB')
-
+const cookieParser = require('cookie-parser');
 const app = express();
 const authRoutes = require('./routes/authroutes'); 
 const { json } = require('body-parser');
-
+const {requireAuth} = require('./middleware/authmiddleware')
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -16,7 +17,7 @@ app.set('view engine', 'ejs');
 
 // routes
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies',requireAuth, (req, res) => res.render('smoothies'));
 
 
 app.use(authRoutes);
@@ -25,3 +26,6 @@ connectToDb()
 app.listen(3000,()=>{
   console.log('Connected to Server 3000')
 })
+
+
+
